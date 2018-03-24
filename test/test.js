a905904f9c4301ba436930588deb4b09dd09ce7d
smthom05@gmail.com <smthom05@gmail.com>
2018-03-24 10:32:45 -0600

    worked on some testing stuff

--- test/test.js
+++ test/test.js
@@ -1,28 +1,50 @@
 var expect = require("chai").expect;
 var assert = require("chai").assert;
-//NOTE: assert is NOT chainable!!! I am more familiar with it though
+var sinon = require("sinon");
+var alpacaBag = require("../public/assets/javascript/survey");
+var xhr, requests;
 
-var multiply = function (x, y) {
-    if (typeof x !== "number" || typeof y !== "number") {
-        throw new Error("x or y is not an integer.");
-    }
-    else return x * y;
-};
-// multiply(2, 4);
+before(function () {
+    xhr = sinon.useFakeXMLHttpRequest();
+    requests = [];
+    xhr.onCreate = function (req) { requests.push(req); };
+});
 
-//test for the multiply function
-describe("multiply", function () {
-    it("should multiply properly when passed numbers", function () {
-        expect(multiply(2, 4)).to.equal(8);
+after(function () {
+    // Like before we must clean up when tampering with globals.
+    xhr.restore();
+});
+
+it("makes a GET request for destinations", function () {
+    alpacaBag(42, sinon.spy());
+
+    assert.equals(requests.length, 1);
+    assert.match(requests[0].url, "/api/destinations");
+});
+
+
+
+//test for the alpacaBag function
+describe("alpacaBag", function () {
+    it("should loop through our cities' results and identify the closest match", function () {
+        expect(alpacaBag(35)).to.equal("Istanbul");
     });
 
-    it("should throw error when not passed numbers", function () {
-        expect(function () {
-            multiply("6", "4");
-        }).to.throw(Error);
+    it("should loop through our cities' results and identify the closest match", function () {
+        expect(alpacaBag(27)).to.equal("Cape Town");
     });
 
-    it("should return the integer eight", function () {
-        assert.equal(8, '8', '== coerces values to strings');
-    })
+    it("should loop through our cities' results and identify the closest match", function () {
+        expect(alpacaBag(50)).to.equal("New York City");
+    });
+
+    // it("should throw error when not passed numbers", function () {
+    //     expect(function () {
+    //         alpacaBag("6", "4");
+    //     }).to.throw(Error);
+    // });
+    //
+    // it("should return the integer eight", function () {
+    //     assert.equal(8, '8', '== coerces values to strings');
+    // })
 });
