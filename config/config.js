b8785c82051e6cfe82007f6084d370d5e7120837
Michael Bolens <bolensmichael@users.noreply.github.com>
2018-03-24 10:27:30 -0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: "root",
-    password: "process.env.DB_PASSWORD",
+    password: process.env.DB_PASSWORD,
     database: "travel_db",
     host: "localhost",
     dialect: "mysql"
