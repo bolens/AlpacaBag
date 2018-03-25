9a96663393eae936c01a52540c5ecd2af9c6542a
smthom05@gmail.com <smthom05@gmail.com>
2018-03-25 14:24:45 -0600

    handlebars

--- controllers/travelersController.js
+++ controllers/travelersController.js
@@ -3,28 +3,16 @@ var router = express.Router();
 var db = require("../models");
 
 // get route -> main
-router.get("/", function (req, res) {
-    res.render("index");
-    // res.redirect("/login_page");
+router.get("/", function(req, res) {
+  res.render("index");
 });
 
-//   var placeObject = {
-//   title: req.body.title,
-//   body: req.body.body,
-//   category: req.body.category
-//   };
-//     res.render("main");
-//     res.send("HOMEPAGE");
-//     res.redirect("/login_page");
-// });
+
 
 router.get("/survey", function(req, res) {
-  // var surveyObject = {
-  //     destination: data
-  //   };
-    res.render("index");
-    res.send("SURVEY");
-    // res.redirect("/login_page");
+  res.render("index");
+  res.send("SURVEY");
+  // res.redirect("/login_page");
 });
 
 // POST route for saving a new user survey
@@ -34,27 +22,26 @@ router.post("/api/user", function(req, res) {
       email: req.body.email,
       surveyResults: req.body.surveyResults
     })
-  .then(function(result) {
-    res.json(result);
-  });
+    .then(function(result) {
+      res.json(result);
+    });
 });
 
 // GET route for getting all of the destinations
 router.get("/api/destinations", function(req, res) {
   console.log(req.body)
-  db.Destination.findAll({
-  }).then(function(result) {
+  db.Destination.findAll({}).then(function(result) {
     res.json(result);
   });
 });
 
 // Get route for retrieving our points of interest from a destination
 router.get("/api/poi/:destination", function(req, res) {
- db.PoI.findAll({
-      where: {
-        destination: req.params.destination
-        },
-      include: [db.Destination]
+  db.PoI.findAll({
+    where: {
+      destination: req.params.destination
+    },
+    include: [db.Destination]
   }).then(function(result) {
     console.log(result);
     res.json(result);
@@ -64,15 +51,15 @@ router.get("/api/poi/:destination", function(req, res) {
 
 // Get route for retrieving our points of interest from a destination
 router.get("/api/destinations/:id", function(req, res) {
- db.Destination.findOne({
-      where: {
-        id: req.params.id
-        },
-      include: [db.PoI]
+  db.Destination.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [db.PoI]
   }).then(function(result) {
     console.log(result);
     res.json(result);
-    // res.render("handlebar", cityObject);
+    res.render("destination", cityObject);
   });
 });
 
@@ -80,40 +67,39 @@ router.get("/api/destinations/:id", function(req, res) {
 router.post("/api/destinations/:id", function(req, res) {
   db.PoI.create(
     req.body
-    ).then(function(result) {
-      res.json(result);
-    });
+  ).then(function(result) {
+    res.json(result);
+  });
 });
 
 // PUT route for updating destinations
 router.put("/api/destinations", function(req, res) {
   db.Destination.update(
-    req.body,
-    {
+    req.body, {
       where: {
         id: req.body.id
       }
     }).then(function(result) {
-      res.json(result);
-    });
+    res.json(result);
+  });
 });
 
 // POST route for creating destinations
 router.post("/api/destinations", function(req, res) {
   db.Destination.create(
     req.body
-    ).then(function(result) {
-      res.json(result);
-    });
+  ).then(function(result) {
+    res.json(result);
+  });
 });
 
 // POST route for creating a point of interest
 router.post("/api/poi", function(req, res) {
   db.PoI.create(
     req.body
-    ).then(function(result) {
-      res.json(result);
-    });
+  ).then(function(result) {
+    res.json(result);
+  });
 });
 
 module.exports = router;
